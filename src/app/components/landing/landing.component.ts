d8545e58f9df2bc7fcdfa9d8ac63c4a194ff7744
Tchaka Spierings <tchaka.spierings@gmail.com>
2017-05-06 19:55:36 +0200

    Add fade-out animation to landing page

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -1,13 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, trigger, state, transition, animate, style } from '@angular/core';
 import { AuthService } from '../../services/auth/auth.service';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.scss']
+  styleUrls: ['./landing.component.scss'],
+  animations: [
+        trigger('landingState', [
+            state('active', style({opacity: '1'})),
+            transition('active => inactive', [
+              animate(250, style({opacity: '0'}))
+            ])
+        ])
+    ]
 })
 export class LandingComponent implements OnInit {
+  private state: string = 'active';
 
   constructor(private _authService: AuthService, private _router: Router) { }
 
@@ -17,8 +26,14 @@ export class LandingComponent implements OnInit {
     }    
   }
 
-  onEnter() {
-    this._router.navigate(['tracker']);
-    this._authService.loggedIn = true;    
+  onEnter() {    
+    this.state = 'inactive';
+  }
+
+  animationDone(animEvent: any) {
+    if(animEvent.toState === 'inactive') {
+      this._authService.loggedIn = true;    
+      this._router.navigate(['tracker']);
+    }    
   }
 }

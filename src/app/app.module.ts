bbaca5082e14fc42dfc19b162a9bbf47c30f2839
Tchaka Spierings <tchaka.spierings@gmail.com>
2017-05-06 19:08:15 +0200

    Add landing page, skeletons for other pages and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app/app.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { TrackerComponent } from './components/tracker/tracker.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AuthService } from './services/auth/auth.service';
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: 'landing', pathMatch: 'full'},
+  { path: 'landing', component: LandingComponent },
+  { path: 'tracker', component: TrackerComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    TrackerComponent,
+    HeaderComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/landing/landing.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-landing',
+  templateUrl: './landing.component.html',
+  styleUrls: ['./landing.component.scss']
+})
+export class LandingComponent implements OnInit {
+
+  constructor(private _authService: AuthService, private _router: Router) { }
+
+  ngOnInit() {
+  }
+
+  onEnter() {
+    this._router.navigate(['tracker']);
+    this._authService.showHeader = true;    
+  }
+}

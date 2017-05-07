441a704172cd48bd750902ddbd27b6d054ab73e5
Tchaka Spierings <tchaka.spierings@gmail.com>
2017-05-07 11:38:40 +0200

    Add fadeout animation for confirmation dialog

--- src/app/components/confirmation-dialog/confirmation-dialog.component.ts
+++ src/app/components/confirmation-dialog/confirmation-dialog.component.ts
@@ -1,14 +1,24 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, trigger, state, transition, style, animate } from '@angular/core';
 
 @Component({
   selector: 'app-confirmation-dialog',
   templateUrl: './confirmation-dialog.component.html',
-  styleUrls: ['./confirmation-dialog.component.scss']
+  styleUrls: ['./confirmation-dialog.component.scss'],
+  animations: [
+        trigger('fadeState', [
+            state('active', style({opacity: '1'})),
+            transition('active => inactive', [
+              animate(100, style({opacity: '0'}))
+            ])
+        ])
+    ]
 })
 export class ConfirmationDialogComponent implements OnInit {
 
   @Input() text: string;
-  @Output() result = new EventEmitter();
+  @Output() resultEvent = new EventEmitter();
+  private result: boolean = false;
+  private state: string = 'active';
 
   constructor() { }
 
@@ -16,10 +26,18 @@ export class ConfirmationDialogComponent implements OnInit {
   }
 
   onYes() {
-    this.result.emit(true);
+    this.result = true;
+    this.state = 'inactive';
   }
 
   onNo() {
-    this.result.emit(false);
+    this.result = false;
+    this.state = 'inactive';
+  }
+
+  animationDone(animEvent: any) {
+    if(animEvent.toState === 'inactive') {
+      this.resultEvent.emit(this.result);
+    }
   }
 }
